a0e639a27fc7e08fc01cf8356c9b833d1a37ce04
RK905 <mr.rayenk@gmail.com>
2015-11-24 21:56:38 -0500

    Initial Commit

---
+++ PocketMS Pal/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PocketMS Pal
+//
+//  Created by Rayen Kamta on 11/24/15.
+//  Copyright © 2015 Geeksdobyte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
